dd0e7236d77318bdb4c456a00b91c901fcbb49e4
arvinddas <arvind.das@firstcry.com>
2017-04-06 18:10:39 +0530

    Update script3.sql

--- script3.sql
+++ script3.sql
@@ -1,2 +1,8 @@
 use firstcry;
-drop table pagemaster;
+create table table_name (
+  ID int not null,
+  name varchar(100) null);
+  
+  update usermaster set emailaddress='arvind@firstcry.com' where userid=1;
+  
+  insert into table_name (1,'arvind');
