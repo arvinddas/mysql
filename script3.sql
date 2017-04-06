bf433c89a7317356c801a68ef43177e28781bbd5
arvinddas <arvind.das@firstcry.com>
2017-04-06 18:13:27 +0530

    Update script3.sql

--- script3.sql
+++ script3.sql
@@ -1,8 +1,6 @@
 use firstcry;
-create table table_name (
-  ID int not null,
-  name varchar(100) null);
+ -- create table table_name (  ID int not null,  name varchar(100) null);
   
-  update usermaster set emailaddress='arvind@firstcry.com' where userid=1;
+  -- update usermaster set emailaddress='arvind@firstcry.com' where userid=1;
   
-  insert into table_name (1,'arvind');
+  insert into table_name (ID,name) values (1,'arvind');
